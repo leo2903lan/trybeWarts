41f9cacbe414b71ccff534044b367bf849dacb49
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-22 17:12:26 -0300

    refa torando as funções para arrow function, direto dentro do eventListner

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const checkFinal = document.getElementById('agreement');
 const button = document.getElementById('submit-btn');
-
-function activeButton() {
-  if (checkFinal.checked) {
-    button.removeAttribute('disabled');
-  }
-}
-checkFinal.addEventListener('click', activeButton);
-
 const login = document.getElementById('login');
 const senha = document.getElementById('senha');
 const entrar = document.getElementById('entrar');
 
-function validar() {
+checkFinal.addEventListener('click', () => {
+  if (checkFinal.checked) {
+    button.removeAttribute('disabled');
+  }
+});
+
+entrar.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
-}
-entrar.addEventListener('click', validar);
+});
