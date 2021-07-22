37727280d2af6145cabc311fb3409c1dd920f03f
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-22 15:45:42 -0300

    requisito 3, verificando se senha e e mail conferem e exibidno alert

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function activeButton() {
   }
 }
 checkFinal.addEventListener('click', activeButton);
+
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
+const entrar = document.getElementById('entrar');
+
+function validar() {
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+  alert('Login ou senha inválidos.');
+}
+}
+entrar.addEventListener('click', validar);
